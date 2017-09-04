4d714453322f238ca8747c1a329c369bf00920bb
huangming1994 <1157114498@qq.com>
2017-09-04 14:59:24 +0800

    add lifecycle

--- src/HomePage.js
+++ src/HomePage.js
@@ -1,6 +1,18 @@
 import React, { Component } from 'react'
 
 class HomePage extends Component {
+  componentWillMount() {
+    console.log('HomePage componentWillMount')
+  }
+  componentDidMount() {
+    console.log('HomePage componentDidMount')
+  }
+  componentWillReceiveProps() {
+    console.log('HomePage componentWillReceiveProps')
+  }
+  componentWillUnmount() {
+    console.log('HomePage componentWillUnmount')
+  }
   render() {
     return (
       <div>这是HomePage</div>
--- src/InfoPage.js
+++ src/InfoPage.js
@@ -1,6 +1,18 @@
 import React, { Component } from 'react'
 
 class InfoPage extends Component {
+  componentWillMount() {
+    console.log('InfoPage componentWillMount')
+  }
+  componentDidMount() {
+    console.log('InfoPage componentDidMount')
+  }
+  componentWillReceiveProps() {
+    console.log('InfoPage componentWillReceiveProps')
+  }
+  componentWillUnmount() {
+    console.log('InfoPage componentWillUnmount')
+  }
   render() {
     return (
       <div>这是InfoPage</div>
